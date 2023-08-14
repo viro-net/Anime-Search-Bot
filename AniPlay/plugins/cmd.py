3c3ccfcad1c2e7682de51477bafe263dc93ca5be
nero-bc <126649764+nero-bc@users.noreply.github.com>
2023-08-14 09:01:39 +0530

    Update cmd.py

--- AniPlay/plugins/cmd.py
+++ AniPlay/plugins/cmd.py
@@ -6,10 +6,10 @@ from AniPlay.plugins.button import BTN
 from AniPlay.plugins.stats import day, over
 
 
-@app.on_message(filters.command(['start', 'ping', 'help', 'alive']))
+@app.on_message(filters.command(['start', '']))
 async def start(_, message: Message):
     try:
-        await message.reply_text('Bot Is Online...\nSearch Animes Using /search or /s\n\n Join - @GreyMatter_Bots for More...')
+        await message.reply_text('जय श्री राम🚩,\n\n**I‘m Anime Search Bot, Creator- @StupidBoi69.**\n\nClick on /help to learn **How to use?** ')
     except:
         return
 
@@ -17,13 +17,13 @@ async def start(_, message: Message):
 QUERY = '**Search Results:** `{}`'
 
 
-@app.on_message(filters.command(['search', 's']))
+@app.on_message(filters.command(['search', 's', 'gimme', 'iwant', 'find', 'anime']))
 async def searchCMD(_, message: Message):
     try:
         user = message.from_user.id
         query = ' '.join(message.command[1:])
         if query == '':
-            return await message.reply_text('Give me something to search ^_^')
+            return await message.reply_text('Give me something to search; after cmd =⁠_⁠= ')
         data = AnimeDex.search(query)
         button = BTN.searchCMD(user, data, query)
         await message.reply_text(QUERY.format(query), reply_markup=button)
@@ -37,12 +37,12 @@ async def searchCMD(_, message: Message):
 @app.on_message(filters.command('stats'))
 async def stats(_, message: Message):
     try:
-        await message.reply_text('Use /stats1 For Day Wise Stats\nAnd /stats2 For Overall Stats')
+        await message.reply_text('Use /stats1, /todaystats For Day Wise Stats\nAnd /stats2, /overallstats For Overall Stats')
     except:
         return
 
 
-@app.on_message(filters.command('stats1'))
+@app.on_message(filters.command('stats1', 'todaystats'))
 async def stats1(_, message: Message):
     try:
         img = day()
@@ -51,10 +51,18 @@ async def stats1(_, message: Message):
         return
 
 
-@app.on_message(filters.command('stats2'))
+@app.on_message(filters.command('stats2', 'overallstats'))
 async def stats2(_, message: Message):
     try:
         img = over()
         await message.reply_photo(img, caption='**AnimeDex | Overall Stats**')
+    except:
+        return
+
+
+@app.on_message(filters.command(['help', '']))
+async def help(_, message: Message):
+    try:
+        await message.reply_text('To Get Started, Simply Use\n\n(**/search ‹anime name›,\n/find ‹anime name›,\n/iwant ‹anime name›.**)\n\n(**HINT: Choose Server5 for Downloading.**')
     except:
         return
