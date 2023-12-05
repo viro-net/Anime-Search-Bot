d0540ab6865e18b7b6bf72ca9300a8d117877b2a
veer naal <140493234+viro-net@users.noreply.github.com>
2023-12-05 08:41:48 +0530

    Update cmd.py

--- AniPlay/plugins/cmd.py
+++ AniPlay/plugins/cmd.py
@@ -3,58 +3,48 @@ from pyrogram import filters
 from AniPlay import app
 from AniPlay.plugins.AnimeDex import AnimeDex
 from AniPlay.plugins.button import BTN
-from AniPlay.plugins.stats import day, over
+from AniPlay.plugins.ErrorHandler import CMDErrorHandler
 
 
-@app.on_message(filters.command(['start', 'ping', 'help', 'alive']))
+@app.on_message(filters.command(["start", "ping", "help", "alive"]))
+@CMDErrorHandler
 async def start(_, message: Message):
     try:
-        await message.reply_text('Bot Is Online...\nSearch Animes Using /search or /s\n\n Join - @GreyMatter_Bots for More...')
+        await message.reply_text(
+            "Bot Is Online...\n\nSearch Animes Using /search or /s"
+        )
     except:
         return
 
 
-QUERY = '**Search Results:** `{}`'
+QUERY = "**Search Results:** `{}`"
 
 
-@app.on_message(filters.command(['search', 's']))
+@app.on_message(filters.command(["search", "s"]))
+@CMDErrorHandler
 async def searchCMD(_, message: Message):
     try:
         user = message.from_user.id
-        query = ' '.join(message.command[1:])
-        if query == '':
-            return await message.reply_text('Give me something to search ^_^')
+        query = " ".join(message.command[1:])
+        if query == "":
+            return await message.reply_text("Give me something to search ^_^")
         data = AnimeDex.search(query)
         button = BTN.searchCMD(user, data, query)
-        await message.reply_text(QUERY.format(query), reply_markup=button)
+        await message.reply_text(
+            f"{QUERY.format(query)}\n\n© {message.from_user.mention}",
+            reply_markup=button,
+        )
     except Exception as e:
+        print(e)
         try:
-            return await message.reply_text('**Anime Not Found...**\n\nProbably Incorrect Name, Try again')
+            return await message.reply_text(
+                "**Anime Not Found...**\n\nProbably Incorrect Name, Try again"
+            )
         except:
             return
 
 
-@app.on_message(filters.command('stats'))
-async def stats(_, message: Message):
-    try:
-        await message.reply_text('Use /stats1 For Day Wise Stats\nAnd /stats2 For Overall Stats')
-    except:
-        return
-
-
-@app.on_message(filters.command('stats1'))
-async def stats1(_, message: Message):
-    try:
-        img = day()
-        await message.reply_photo(img, caption='**AnimeDex | Day Wise Stats**')
-    except:
-        return
-
-
-@app.on_message(filters.command('stats2'))
-async def stats2(_, message: Message):
-    try:
-        img = over()
-        await message.reply_photo(img, caption='**AnimeDex | Overall Stats**')
-    except:
-        return
+@app.on_message(filters.command(["report"]))
+@CMDErrorHandler
+async def reportCMD(_, message: Message):
+    await message.reply_text("Report Bugs Here: @TechZBots_Support")
